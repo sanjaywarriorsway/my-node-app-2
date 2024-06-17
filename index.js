2fd24191a7ebff5e38d34882f9eafc4ce469b38d
sanjaywarriorsway <warriorswayhub@gmail.com>
2024-06-17 15:55:38 +0530

    Add CD workflow modified final

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, WarriorsWay Community');
+  res.send('Hello, WarriorsWay Community !');
 });
 
 app.listen(PORT, () => {
