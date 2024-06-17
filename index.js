48ac98fc513f79352c85f26a2d41ece574197c45
sanjaywarriorsway <warriorswayhub@gmail.com>
2024-06-17 15:59:39 +0530

    final demo modification on ci cd

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, WarriorsWay Community !');
+  res.send('Hello, WarriorsWay Community ! Welcome to Full Stack web dev. Series');
 });
 
 app.listen(PORT, () => {
